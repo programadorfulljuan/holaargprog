6a6e35b779bfe980b96b4dee9706c23c93c7828f
programadorfulljuan <programadorfulljuan@gmail.com>
2022-09-07 05:36:26 -0300

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,11 @@
+// Noindex: le dice a un motor de búsqueda que no indexe una página.
+
+// Index: Le dice a un motor de búsqueda que indexe una página. Tenga en cuenta que no necesita agregar esta metaetiqueta; es el predeterminado.
+
+// Seguir: Incluso si la página no está indexada, el rastreador debe seguir todos los enlaces en una página y pasar equidad a las páginas enlazadas.
+
+// Nofollow: le dice a un rastreador que no siga ningún enlace en una página ni transmita ninguna equidad de enlace.
+
+// Noimageindex: le dice a un rastreador que no indexe ninguna imagen en una página.
+
+// Ninguno: Equivale a usar las etiquetas noindex y nofollow simultáneamente.
